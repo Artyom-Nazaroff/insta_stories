cba862084df9349f08b1ae3fa8fc9cfa87745486
Artyom Nazarov <artyom.nazaroff@yandex.ru>
2022-03-25 11:47:02 +0300

    refactor library

---
+++ index.js
@@ -0,0 +1,10 @@
+initPlayer({
+    target: '.my-player',
+    slides: [
+        {url: 'img/chunk-1.jpg', alt: 'slide1'},
+        {url: 'img/chunk-2.jpg', alt: 'slide2'},
+        {url: 'img/chunk-3.jpg', alt: 'slide3'},
+        {url: 'img/chunk-4.jpg', alt: 'slide4'},
+    ],
+    delayPerSlide: 5,
+});
